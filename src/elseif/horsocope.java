ea03fa22f68fb6a8188dc34c5a0402a1ebf97551
andrew983 <league@worthymini-5.attlocal.net>
2018-06-02 12:53:11 -0700

    ghrorscope+yalive comp

--- src/elseif/horsocope.java
+++ src/elseif/horsocope.java
@@ -6,70 +6,101 @@ public class horsocope {
 
 	public static void main(String[] args) {
 		
-		String zodiac = JOptionPane.showInputDialog("wh0ts youR zodi@c s|gn");
+		
+		for(int i=0;i<10;i++) {
 		
 		
+		String zodiac = JOptionPane.showInputDialog("wh0ts youR zodi@c s|gn?(Aries, Taurus, Gemini, Cancer, Leo, "
+				+ "Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces)");
 		
+		if(zodiac.equals("Aries")) {
+			
+			JOptionPane.showMessageDialog(null, "buy some pants");
+			
+		}
+		
+else if(zodiac.equals("Taurus")) {
+			
+			JOptionPane.showMessageDialog(null, "if smoking is BAD for you why does it CURE salmon?");
+			
+		}
+		
+else if(zodiac.equals("Gemini")) {
 	
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
+	JOptionPane.showMessageDialog(null, "sledding is fun?");
+	
+}
 
+else if(zodiac.equals("Cancer")) {
+	
+	JOptionPane.showMessageDialog(null, "oyster sauce+srachiaeuhl sauce+bean sprouts+pHO = a match made in heaven. Try today!");
+	
+}
+
+else if(zodiac.equals("Leo")) {
+	
+	JOptionPane.showMessageDialog(null, "You will invent the world's first instant steak.");
+	
+}
+
+else if(zodiac.equals("Virgo")) {
+	
+	JOptionPane.showMessageDialog(null, "justbuild.lol/play");
+	
+}
+
+else if(zodiac.equals("Libra")) {
+	
+	JOptionPane.showMessageDialog(null, "Type in Scorpio.");
+	
+}
+
+else if(zodiac.equals("Scorpio")) {
+	
+	JOptionPane.showMessageDialog(null, "Type in Libra.");
+	
+}
+
+else if(zodiac.equals("Sagittarius")) {
+	
+	JOptionPane.showMessageDialog(null, "Holding the coconut with a towel, firmly tap it with a meat mallet or hammer, turning as needed, until the shell starts cracking in half. Split the shell, then put the coconut cut side down on a flat surface. Tap with the mallet to loosen the flesh. Carefully pry the flesh away from the shell with a butter knife.");
+	
+}
+
+else if(zodiac.equals("Capricorn")) {
+	
+	JOptionPane.showMessageDialog(null, "is apricorn a thing");
+	
+}
+
+else if(zodiac.equals("Aquarius")) {
+	
+	JOptionPane.showMessageDialog(null, "u r swims");
+	
+}
+
+else if(zodiac.equals("Pisces")) {
+	
+	JOptionPane.showMessageDialog(null, "reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+	
+}
+
+
+		
+else {		
+
+
+JOptionPane.showMessageDialog(null,"ARE U STUD U RETARTED SAHT");
+
+
+
+}
+
+		
+		
+		
+		
+		
+		}
+	}
 }
---
+++ src/loops/yearsalive.java
@@ -0,0 +1,15 @@
+package loops;
+
+import javax.swing.JOptionPane;
+
+public class yearsalive {
+public static void main(String[] args) {
+	
+	
+	for(int i=2004;i<2019;i++) {
+		System.out.println(i);
+		
+
+}
+}
+}
