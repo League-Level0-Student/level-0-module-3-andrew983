ce4eadb8d741fbc707cbd93e965f1ec574e740c6
League <League@AdminisorsiMac2.attlocal.net>
2018-06-23 12:55:56 -0700

    WoF and fB complete
    :)

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -9,12 +9,31 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
+		rob.penDown();
+		
+		rob.setSpeed(400);
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+rob.setX(40);
 		// 2. Make the robot draw a star shape. Hint: 144.
+for (int i = 0; i < 20; i++) {
+	
 
-		// 3. Set the size of the star to 30.
+for (int i1 = 0; i1 < 5; i1++) {
+	rob.turn(144);
+rob.move(30);
+	
+}
+rob.penUp();
+rob.turn(90);
+
+rob.move(50);
+rob.penDown();
+rob.setAngle(360);
+	}
+	}
+}
+// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
@@ -24,6 +43,6 @@ public class WalkOfFame {
 		 * Hint: The distance between stars is 50.
 		 */
 
-	}
+	
 
-}
+
